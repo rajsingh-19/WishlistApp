50bb969be6800dbfb1cf76e48206e918fc8f04b4
RajSingh <rajsingh.19.vns@gmail.com>
2024-01-30 21:33:51 +0530

    Added html, css, and js file

---
+++ index.js
@@ -0,0 +1,38 @@
+let todoInput = document.querySelector(".input");
+todoInput.value = "";
+let addBtn = document.querySelector(".btn");
+let todosContainer = document.querySelector(".todosContainer");
+
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault(); // it stops to do the default operation of form i.e., submition
+    todoInput.value = todoInput.value.trim();
+    if(todoInput.value.length > 0) {
+        addToTodos();
+        todoInput.value = "";
+    }
+});
+
+function addToTodos() {
+    let todoList = document.createElement("div");
+    todoList.id = "delId";
+    let todoListInp = document.createElement("input");
+    todoListInp.setAttribute("type", "checkbox"); 
+    todoListInp.classList.add("inpCheck");
+    let todoListLabel = document.createElement("label");
+    todoListLabel.innerText = todoInput.value; 
+    todoListLabel.classList.add("labels");
+    let todoListBtn = document.createElement("button"); 
+    todoListBtn.innerText = "Del";
+    todoListBtn.classList.add("delBtn");
+
+    todoListBtn.addEventListener("click", handleDelBtn);
+
+    todoList.appendChild(todoListInp);
+    todoList.appendChild(todoListLabel);
+    todoList.appendChild(todoListBtn);
+    todosContainer.appendChild(todoList);
+}
+function handleDelBtn () {
+    let delTodo = document.getElementById("delId");
+    delTodo.remove();
+}
